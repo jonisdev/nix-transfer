f56b775e790ae77210036bec903593b9f9b54b28
Jonatan Vianna <jonatan.vianna@gmail.com>
2018-04-29 00:00:47 -0300

    Transfer model implemented

--- transfer/models.py
+++ transfer/models.py
@@ -1,23 +1,25 @@
 from django.db import models
+from model_utils import Choices
+from datetime import datetime as dt
 
 
 class Transfer(models.Model):
-    CC = 'CC'
-    TED = 'TED'
-    DOC = 'DOC'
+    # CC = 'CC'
+    # TED = 'TED'
+    # DOC = 'DOC'
 
-    TYPE_CHOICES = (
-        (CC, 'Conta Corrente'),
-        (TED, 'Transfêrencia Eletrônica de Documentos'),
-        (DOC, 'Documento de Ordem de Crédito'),
+    TYPE_CHOICES = Choices(
+        ('CC', 'Conta Corrente'),
+        ('TED', 'Transfêrencia Eletrônica de Documentos'),
+        ('DOC', 'Documento de Ordem de Crédito'),
     )
 
-    OK = 'OK'
-    ERROR = 'ERROR'
+    # OK = 'OK'
+    # ERROR = 'ERROR'
 
-    STATUS_CHOICES = (
-        (OK, 'OK'),
-        (ERROR, 'Erro'),
+    STATUS_CHOICES = Choices(
+        ('OK', 'OK'),
+        ('ERROR', 'Erro'),
     )
 
     # TODO - Implement custom user
@@ -74,17 +76,49 @@ class Transfer(models.Model):
         choices=STATUS_CHOICES
     )
 
+    def set_transfer_type(self):
 
-t = Transfer(user_id=222,
-              paying_name ='Jonatan Vianna da Silva',
-              paying_bank=1,
-              paying_agency=3334,
-              paying_account=188003,
-              beneficiary_name='Jonatan Vianna da Silva',
-              beneficiary_bank=1,
-              beneficiary_agency=3334,
-              beneficiary_account=188003,
-              amount=9237651239872.97,
-              type=Transfer.TYPE_CHOICES[0][1],
-              status=Transfer.STATUS_CHOICES[0][0],
-              )
+        current_datetime = dt.now()
+        current_time = dt.time(current_datetime)
+        current_date = current_datetime.date()
+        initial_ted_time = dt.time(dt(
+            current_date.year, current_date.month, current_date.day, 10, 0, 0))
+        final_ted_time = dt.time(dt(
+            current_date.year, current_date.month, current_date.day, 16, 0, 0))
+
+        if self.paying_bank == self.beneficiary_bank:
+            self.type = self.TYPE_CHOICES.CC
+            self.save()
+        elif dt.weekday(current_datetime) in (5, 6):
+            self.type = self.TYPE_CHOICES.DOC
+            self.save()
+        elif current_time > initial_ted_time and \
+                current_time < final_ted_time and \
+                self.amount < 5000.0:
+            self.type = self.TYPE_CHOICES.TED
+            self.save()
+        else:
+            self.type = self.TYPE_CHOICES.DOC
+            self.save()
+
+    def set_transfer_status(self):
+        if self.amount > 100000.0:
+            self.status = self.STATUS_CHOICES.ERROR
+            self.save()
+        else:
+            self.status = self.STATUS_CHOICES.OK
+            self.save()
+
+
+# from transfer.models import Transfer
+# t = Transfer(user_id=222,
+#               paying_name ='Jonatan Vianna da Silva',
+#               paying_bank=1,
+#               paying_agency=3334,
+#               paying_account=188003,
+#               beneficiary_name='Jonatan Vianna da Silva',
+#               beneficiary_bank=1,
+#               beneficiary_agency=3334,
+#               beneficiary_account=188003,
+#               amount=9237651239872.97,
+#               )
--- transfer/scripts.py
+++ transfer/scripts.py
@@ -1,27 +1,3 @@
 from .models import Transfer
 from datetime import datetime as dt
 import calendar
-
-
-def validate_transction_type(transferency):
-
-    current_datetime = dt.now()
-
-    current_time = dt.time(current_datetime)
-    current_date = current_datetime.date()
-    initial_ted_time = dt.time(dt(
-        current_date.year, current_date.month, current_date.day, 10, 0, 0))
-    final_ted_time = dt.time(dt(
-        current_date.year, current_date.month, current_date.day, 16, 0, 0))
-
-    if transferency.paying_bank == transferency.beneficiary_bank:
-        return transferency.type == transferency.TYPE_CHOICES.CC
-    elif dt.weekday(current_datetime) in (5, 6):
-        return transferency.type == transferency.TYPE_CHOICES.DOC
-    elif current_time > initial_ted_time and current_time < final_ted_time and \
-            transferency.amount < 5000.0:
-        return transferency.type == transferency.TYPE_CHOICES.TED
-    else:
-        return transferency.type == transferency.TYPE_CHOICES.DOC
-
-
